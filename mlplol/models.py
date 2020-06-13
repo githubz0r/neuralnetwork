c245de74f84af2cda2fb8b5893124f82cef29d0c
githubz0r <lassprite@hotmail.com>
2020-06-13 09:00:47 +0200

    cleaned a bit

---
+++ mlplol/models.py
@@ -0,0 +1,141 @@
+import numpy as np
+
+def neural_network(x, w, activation_function, classify = False, has_ones = False):
+    w0 = w[0]
+    w1 = w[1]
+    if len(x.shape)<1:
+        x = x.reshape(1,1)
+    N = x.shape[0]
+    if not has_ones:
+        x = np.c_[np.ones(N), x]
+    a = x@w0 # dimenson is d*M where M is number of columns in w0, i.e. number of neurons
+    if activation_function == 'relu':
+        z = relu(a)
+    elif activation_function == 'softsign':
+        z = a/(1+np.abs(a))
+    z = np.c_[np.ones(z.shape[0]), z] # ones for bias
+    a2 = z@w1
+    if not classify:
+        z2 = z@w1
+    else:
+        z2 = exp(z)/(exp(z)+1)
+    return(dict(first_mult = a, first_mult_nonlin = z, second_mult = a2, output = z2))
+
+def relu(x):
+    zeroes = np.zeros(x.shape)
+    zeroes[x>0] = x[x>0]
+    return(zeroes)
+
+def relu_grad(x):
+    grad = (x > 0)*1
+    return(grad)
+
+
+def nn_grad(x, y, a, z, out, w, activation_function):
+    '''y must be an array of dimension at least 2'''
+    w_out = w[1]
+    N = x.shape[0]
+    delta_outs = out-y # N*1
+    #delta_outs_repeated = np.repeat((out-y), w_out.shape[0], axis=1) # N*(n_hidden+1)
+    output_grad = np.sum(np.multiply(delta_outs, z), axis=0)/N # gradient of output unit
+    if len(output_grad.shape)<2:
+        output_grad = output_grad.reshape(output_grad.shape[0], 1)
+    if activation_function == 'relu':
+        hidden_activation_deriv = relu_grad(a)
+    elif activation_function == 'softsign':
+        hidden_activation_deriv = 1/((1+np.abs(a))**2)
+    delta_hidden_sum_parts = delta_outs@w_out.T # this must be a sum with more than 1 output neuron
+    delta_hidden_sum_parts = delta_hidden_sum_parts[:,1:] # removing bias column
+    delta_hidden = hidden_activation_deriv*delta_hidden_sum_parts
+    hidden_grad = 0
+    for i in range(N):
+        vector_of_deltas = delta_hidden[i,:]
+        vector_of_deltas = vector_of_deltas.reshape(a.shape[1],1)
+        grad_element = x[i,:]*vector_of_deltas
+        hidden_grad += grad_element/N
+    return([hidden_grad.T, output_grad])
+
+def nn_gradient_descent(x_train, y_train, x_val, y_val, n_hidden, rate, iterations, patience,
+                       verbose, weights, initialization_factors, activation_function):
+    if len(y_train.shape)==1:
+        y_train = y_train.reshape(y_train.shape[0], 1)
+    if len(y_val.shape)==1:
+        y_val = y_val.reshape(y_val.shape[0], 1)
+    if len(x_train.shape)==1:
+        x_train = x_train.reshape(x_train.shape[0], 1)
+    K = y_train.shape[1]
+    D = x_train.shape[1]
+    N = x_train.shape[0]
+    x_train = np.c_[np.ones(N), x_train, ]
+    # initialize weights
+    if initialization_factors == None:
+        initialization_factors = [np.sqrt(2/(D+1 + n_hidden)), np.sqrt(2/(n_hidden+1 + K))]
+    if weights == None:
+        w_hidden0 = np.random.normal(0, 1, (D+1, n_hidden)) * initialization_factors[0]
+        w_out0 = np.random.normal(0, 1, (n_hidden+1, K)) * initialization_factors[1]
+    w_hidden1 = None
+    w_out1 = None
+    w_best = None
+    
+    patience_counter = patience
+    train_loss = []
+    val_loss = []
+    gradient_norm = []
+    max_iter = iterations
+    while iterations > 0 and patience_counter >0:
+        w=[w_hidden0, w_out0]
+        nn_outs_train = neural_network(x_train, w, activation_function, has_ones=True)
+        a = nn_outs_train['first_mult']
+        z = nn_outs_train['first_mult_nonlin']
+        out = nn_outs_train['output']
+        train_error = 0.5*np.sum((y_train - out)**2)/N
+        if len(out.shape)<2:
+            out=out.reshape(y_train.shape[0], 1) # reshaping to get broadcasting to work later
+        train_loss.append(train_error)
+        grads = nn_grad(x=x_train, y=y_train, a=a, z=z, out=out, w=w, activation_function=activation_function)
+        hidden_grad = grads[0]
+        output_grad = grads[1]
+        w_hidden1 = w_hidden0 - rate*hidden_grad #/N
+        w_out1 = w_out0 - rate*output_grad #/N
+        val_out = neural_network(x_val, w, activation_function=activation_function)['output']
+        val_error = 0.5*np.sum((y_val - val_out)**2)/x_val.shape[0]
+        iterations -= 1
+        if verbose: 
+            print('iterations: ', max_iter-iterations)
+            print('Training loss: {}, Validation loss: {}'.format(train_error, val_error))
+        if len(val_loss)>1:
+            if val_error < min(val_loss):
+                if verbose: 
+                    print('new best w')
+                w_best = w
+                patience_counter = patience
+            elif val_error >= val_loss[-1]:
+                patience_counter -= 1
+        val_loss.append(val_error)
+        w_out0 = w_out1
+        w_hidden0 = w_hidden1
+        gradient_norm_i = np.sqrt(np.sum(np.concatenate([hidden_grad.flatten(), output_grad.flatten()])**2))
+        gradient_norm.append(gradient_norm_i)
+    if w_best == None:
+        w_best = [w_hidden0, w_out0]
+    return(dict(weights=w_best, train_loss=train_loss, val_loss=val_loss, gradient_norm=gradient_norm,
+               iterations=max_iter-iterations))
+
+class NNregressor_onelayer:
+    def __init__(self, activation_function, weights=None):
+        self.weights = weights
+        self.activation_function = activation_function
+    def estimate_weights(self, trainx, trainy, valx, valy, n_hidden, rate, iterations, patience, verbose,
+                        weight_initialization_factors):
+        training_results = nn_gradient_descent(trainx, trainy, valx, valy, n_hidden, 
+                                           rate, iterations, patience, verbose,
+                                           self.weights, weight_initialization_factors,
+                                               activation_function = self.activation_function)
+        self.weights = training_results['weights']
+        self.training_loss = training_results['train_loss']
+        self.validation_loss = training_results['val_loss']
+        self.gradient_norm = training_results['gradient_norm']
+        self.iterations = training_results['iterations']
+    def predict(self, x):
+        predictions = neural_network(x, self.weights, activation_function = self.activation_function)
+        return(predictions['second_mult'].ravel())
---
+++ mlplol/utils.py
@@ -0,0 +1,94 @@
+import numpy as np
+from scipy import optimize
+from .models import neural_network, nn_grad
+import functools
+
+def squared_loss(y1, y2):
+    N = y1.shape[0]
+    if len(y1.shape) > 2:
+        y1 = np.expand_dims(y1, 1)
+    if len(y2.shape) > 2:
+        y2 = np.expand_dims(y2, 1)
+    loss = 0.5*np.sum((y1 - y2)**2)/N
+    return(loss)
+
+def rebuild_from_shapes(flat_vector, shapes):
+    lengths = [0] + [functools.reduce(lambda x,y: x*y, i) for i in shapes]
+    subs = []
+    for i in range(len(lengths)-1):
+        index_1 = lengths[i]
+        index_2 = lengths[i]+lengths[i+1]
+        subs.append(flat_vector[index_1:index_2])
+    for i,_ in enumerate(subs):
+        subs[i] = subs[i].reshape(shapes[i])
+    return(subs)
+
+def nn_flat(w_flat, shapes, x, y, activation_function='relu', classify=False):
+    w_subs = rebuild_from_shapes(w_flat, shapes)
+    outputs = neural_network(x, w_subs, 'relu')['output']
+    if not classify:
+        error = 0.5*np.sum((outputs-y.reshape(y.shape[0], 1))**2)/y.shape[0]
+    else:
+        error = np.sum(y*np.log(outputs)+(1-y)*np.log(1-outputs))/y.shape[0]
+    return(error)
+
+def gradient_checker_(w, x, y, activation_function, eps, classify=False):
+    '''using scipy approx prime, but must flatten weights and concatenate which
+        kinda sucks'''
+    if len(x.shape)<1:
+        x = x.reshape(1,1)
+    if len(y.shape)==1:
+        y = y.reshape(y.shape[0], 1)
+    shapes = [i.shape for i in w]
+    nn_outs = neural_network(x, w, activation_function)
+    a = nn_outs['first_mult']
+    z = nn_outs['first_mult_nonlin']
+    out = nn_outs['output']
+    x_ones = np.c_[np.ones(x.shape[0]), x]
+    true_grad = nn_grad(x_ones, y, a, z, out, w, activation_function)
+    flat_w = np.concatenate([i.flatten() for i in w])
+    approx_grad = optimize.approx_fprime(flat_w, nn_flat, eps, shapes, x, y)
+    approx_grad_split = rebuild_from_shapes(approx_grad, shapes)
+    return(dict(true_grads = true_grad, approx_grads = approx_grad_split))
+
+def calc_approx_grad(w, x, y, eps, true_loss, *args):
+    finite_grads = [np.zeros(w_i.shape) for w_i in w]
+    for k in range(len(w)):
+        w_k = w[k]
+        finite_grad_k = finite_grads[k]
+        for i in np.arange(w_k.shape[0]):
+            for j in np.arange(w_k.shape[1]):
+                w_k_f = np.copy(w_k)
+                w_k_f[i, j] = w_k[i, j]+eps
+                w_list = w[0:k] + [w_k_f] + w[k+1:]
+                output_kij = neural_network(x, w_list, *args)['output']
+                finite_loss = squared_loss(y, output_kij)
+                finite_diff = (finite_loss - true_loss)/eps
+                finite_grad_k[i, j] = finite_diff
+    return(finite_grads)
+
+
+
+def gradient_checker(w, x, y, activation_function='relu', eps = np.sqrt(np.finfo(float).eps), 
+                    classify=False):
+    if len(x.shape)<1:
+        x = x.reshape(1,1)
+    if len(y.shape)==1:
+        y = y.reshape(y.shape[0], 1)
+    nn_outs = neural_network(x, w, activation_function)
+    a = nn_outs['first_mult']
+    z = nn_outs['first_mult_nonlin']
+    out = nn_outs['output']
+    true_error = squared_loss(y, out)
+    x_ones = np.c_[np.ones(x.shape[0]), x]
+    true_grad = nn_grad(x_ones, y, a, z, out, w, activation_function)
+    approx_grad = calc_approx_grad(w, x, y, eps, true_error, activation_function)
+    return(dict(true_grads = true_grad, approx_grads = approx_grad))
+
+def gradient_quotients(w, x, y, eps = np.sqrt(np.finfo(float).eps), 
+                      activation_function='relu', classify=False):
+    gradients = gradient_checker(w, x, y, activation_function, eps, classify)
+    quotients = []
+    for i,j in zip(gradients['true_grads'], gradients['approx_grads']):
+        quotients.append((i+eps)/(j+eps))
+    return(quotients)
